527817891d5da5993e75e9f586b055d51aa6b866
Reu-Amissah <reujoyamissah@gmail.com>
2024-03-04 16:34:46 +0000

    adds some imports

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
 const PORT = process.env.port || 3000;
 const movieController = require("./controllers/MovieController");
 
