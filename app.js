00c402558ac6c894b9085e484196384f267e2b75
Reu-Amissah <reujoyamissah@gmail.com>
2024-03-08 16:14:37 +0000

    feat: add env file and use values

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+//setup ENV.
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 const app = express();
-const passport = require('passport');
-const session = require('express-session');
-const PORT = process.env.port || 3000;
+const passport = require("passport");
+const session = require("express-session");
+const PORT = process.env.PORT || 3000;
 const movieController = require("./controllers/MovieController");
+const store = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24, secure: false },
+    store,
+  })
+);
 
 app.get("/", movieController.get);
 
