d4da967e1f619c8253d623b50f624084c10adc7f
Reu-Amissah <reujoyamissah@gmail.com>
2024-05-23 16:01:19 +0000

    feat: create movie Schema

---
+++ models/movies.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const movieSchema = new Schema({
+  _id: Number,
+  title: String,
+  thumbnail: {
+    trending: {
+      small: String,
+      large: String,
+    },
+    regular: {
+      small: String,
+      medium: String,
+      large: String,
+    },
+  },
+  year: number,
+  category: String,
+  rating: String,
+  isBookmarked: Boolean,
+  isTrending: Boolean,
+});
